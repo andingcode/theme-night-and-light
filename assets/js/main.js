1240c743e28ecbf7a1cdff02cd999da79562c4ae
fluketoselnwza <fluketose.lnwza@gmail.com>
2023-09-05 23:54:08 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+const navIcon = document.getElementById('nav-icon');
+const navTitle = document.getElementById('nav-title');
+
+const txtNight = document.createTextNode("Night");
+const txtLight = document.createTextNode("Light");
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function lightTheme() {
+    setTheme('theme-light')
+    navIcon.classList.add('ri-sun-fill')
+    navIcon.classList.remove('ri-moon-fill')
+    navTitle.innerText = 'Light'
+}
+
+function darkTheme() {
+    setTheme('theme-dark')
+    navIcon.classList.remove('ri-sun-fill')
+    navIcon.classList.add('ri-moon-fill')
+    navTitle.innerText = 'Night'
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        lightTheme()
+    } else {
+        darkTheme()
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark')
+        navIcon.classList.add('ri-moon-fill')
+        navTitle.innerText = 'Night'
+    } else {
+        setTheme('theme-light')
+        navIcon.classList.add('ri-sun-fill')
+        navTitle.innerText = 'Light'
+    }
+})();
